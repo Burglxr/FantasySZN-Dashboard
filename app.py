0e384c0f15f10b93d635727e810f1f1d404aca71
Burglxr <michelzack10@gmail.com>
2026-05-04 14:39:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from trade_engine import calculate_fantasy_points, evaluate_trade
 
 st.set_page_config(page_title='Fantasy Basketball Dashboard', layout='wide')
 
